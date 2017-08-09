6a56906549167601bb94564e6d2297d172427d4d
DESKTOP-5LD9R53\Nitin <iamnrohra@gmail.com>
2017-08-09 16:59:45 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,66 @@
+<link rel="stylesheet" href="component/footer.css">
+             <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+           
+            
+ 
+ 
+ <footer class="footer-distributed foot" id="know" style="margin:0;">
+
+			<div class="footer-left">
+
+				<span>
+                    <a class="navbar-brand" href="index.php"><img src="images/logowhite.png" alt="" width="250px" height="100px">
+                    </a>
+                    </span>
+                    <br>
+
+				<p class="footer-links">
+                    
+					<a href="index.php">Home</a>
+					·
+					<a href="OurTeam.php">About Us</a>
+					·
+					<a href="#contact_us">Contact</a>
+				</p>
+
+				<p class="footer-company-name">Resiowl &copy; 2017</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Fiends colony</span> jaipur, INDIA</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+91 9782154685</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@company.com">support@resiowl.com</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>About the company</span>
+			Our company aims at providing the best place to live or rent for our coustomers
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-github"></i></a>
+
+				</div>
+
+			</div>
+
+		</footer>
---
+++ includes/header2.php
@@ -0,0 +1,48 @@
+<style>
+	.logoimg{
+    top:-2px;
+    position: absolute;
+    height: 50px;
+    width: 120px;
+
+} </style>	
+
+
+<nav class="nb navbar navbar-inverse" style="margin:0px;background-color:teal;border-color:teal;">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+	                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+	                        <span class="sr-only">Toggle navigation</span>
+	                        <span class="icon-bar"></span>
+	                        <span class="icon-bar"></span>
+	                        <span class="icon-bar"></span>
+	                    </button>
+						<a class="navbar-brand" href="index.php"><img class="logoimg" src="images/logowhite.png" alt=""></a>
+                    </div>
+
+                    <div class="collapse navbar-collapse"  id="bs-example-navbar-collapse-2">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a style="color:white" href="#find">Fetch a Nest</a></li>
+							<li><a style="color:white" href="#amenities">Amenities</a></li>
+	                        <li><a style="color:white" href="#Contact_us">Contact Us</a></li>
+	                        <li><a style="color:white" href="form.php">List your property</a></li>
+                            <li><a style="color:white" href="login.php">Login | Register</a></li>
+							<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu"  role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+							</ul>
+                    </div>  
+                </div>
+            </nav>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	      	<script src="component/bootstrap.min.js"></script>
+            
